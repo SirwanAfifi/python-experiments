bba30db44c86569405372a86b06b5479b5b18cda
Sirwan Afifi <sirwanafifi@Sirwans-MacBook-Pro.local>
2022-05-02 08:52:40 +0100

    Fix naming conflict

--- faker/gen.py
+++ faker/gen.py
@@ -1,12 +1,12 @@
 import json
 import random
 import secrets
-import users
+import users as fakeUsers
 
-def generate_users(list, scale = 2):
+def generate_users(scale = 2):
     users = []
     for _ in range(scale):
-        first_name = random.choice(list)
+        first_name = random.choice(fakeUsers.names)
         newUser = {
             'name': first_name,
             'email': str.lower(first_name) + "@python.com",
@@ -15,14 +15,14 @@ def generate_users(list, scale = 2):
         users.append(newUser)
     return users
 
-def generate_tax_data(list, scale = 2):
+def generate_tax_data(scale = 2):
     # Generate Random people and their tax information
     people = []
     for _ in range(scale):
         tax_rate = random.randint(0, 100)
-        amount = random.randint(3000, 6000000)
+        amount = random.randint(3000, 600000)
         newPerson = {
-            'name': random.choice(list),
+            'name': random.choice(fakeUsers.names),
             'tax_rate': tax_rate,
             'amount': int(amount + (amount * (tax_rate / 100))),
         }
@@ -34,8 +34,8 @@ type = int(input("Users or Tax Data?\n 1) Users \n 2) Tax Data\n Enter 1 or 2: "
 scale = int(input("How many people would you like to generate? "))
 
 if type == 1:
-    print(json.dumps(generate_users(users.names, scale), indent=4))
+    print(json.dumps(generate_users(scale), indent=4))
 elif type == 2:
-    print(json.dumps(generate_tax_data(users.names, scale), indent=4))
+    print(json.dumps(generate_tax_data(scale), indent=4))
 
 
